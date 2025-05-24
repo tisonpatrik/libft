d44813ffc3b99959fa045cb6ee34ca8576b2e572
Patrik <ptison@c2r7s1.42prague.com>
2025-05-24 20:36:16 +0200

    strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,29 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ptison <ptison@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/24 19:46:37 by ptison            #+#    #+#             */
-/*   Updated: 2025/05/24 20:34:48 by ptison           ###   ########.fr       */
+/*   Created: 2025/05/24 20:25:02 by ptison            #+#    #+#             */
+/*   Updated: 2025/05/24 20:36:01 by ptison           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-char	*ft_strchr(const char *s, int c)
-{
-	int	i;
+size_t	ft_strlen(const char *s);
 
-	i = 0;
-	while (s[i])
+char	*ft_strrchr(const char *str, int c)
+{
+	size_t	size;
+
+	size = ft_strlen(str);
+	while (str[size - 1])
 	{
-		if (s[i] == (char)c)
+		if (str[size] == (char)c)
 		{
-			return ((char *)&s[i]);
+			return ((char *)&str[size]);
 		}
-		i++;
+		size--;
 	}
 	return (NULL);
 }
@@ -41,10 +43,10 @@ int	main(void)
 	char	*current;
 
 	s = "Hello, my friend.";
-	c = 'f';
-	expected = strchr(s, c);
+	c = 'e';
+	expected = strrchr(s, c);
 	printf("expected: %s \n", expected);
-	current = ft_strchr(s, c);
+	current = ft_strrchr(s, c);
 	printf("current: %s \n", current);
 	assert(expected == current);
 	return (1);

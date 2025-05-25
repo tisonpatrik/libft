3313e67f30b15b4edba455b51ec16d21283db3b8
Patrik <ptison@c2r7s1.42prague.com>
2025-05-25 18:53:19 +0200

    strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ptison <ptison@student.42prague.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/25 17:23:42 by ptison            #+#    #+#             */
+/*   Updated: 2025/05/25 17:23:49 by ptison           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include <stdlib.h>
+
+void		*ft_memcpy(void *dst, const void *src, size_t n);
+size_t		ft_strlen(const char *s);
+char		*ft_strdup(char *src);
+
+char	is_in_set(char c, const char *set)
+{
+	while (*set)
+	{
+		if (*set == c)
+			return (1);
+		set++;
+	}
+	return (0);
+}
+
+size_t	find_start(const char *s1, const char *set)
+{
+	size_t i = 0;
+	while (s1[i] && is_in_set(s1[i], set))
+		i++;
+	return i;
+}
+
+size_t	find_end(const char *s1, const char *set, size_t start)
+{
+	size_t pos = ft_strlen(s1)-1;
+	while (pos > 0 && pos != start && is_in_set(s1[pos], set))
+		pos--;
+	return pos;
+}
+
+char	*ft_strtrim(const char *s1, const char *set)
+{
+	size_t	start;
+	size_t	end;
+	size_t	len;
+	char	*trimmed;
+
+	if (!s1 || !set)
+		return NULL;
+
+	if (set[0] == '\0')
+		return ft_strdup((char *)s1);
+
+	start = find_start(s1, set);
+	end = find_end(s1, set, start);
+
+	if (end < start)
+		len = 0;
+	else
+		len = end - start + 1;
+
+	trimmed = (char *)malloc((len +1)*sizeof(char));
+	if (!trimmed)
+		return NULL;
+
+	ft_memcpy(trimmed, s1 + start, len);
+	trimmed[len] = '\0';
+	return trimmed;
+}
+
+/*
+#include <stdio.h>
+int main(void)
+{
+	const char *s1 = "123";
+	const char *set = "";
+
+	char *result = ft_strtrim(s1, set);
+
+	if (result)
+	{
+		printf("Result: \"%s\"\n", result);
+		free(result);
+	}
+	else
+	{
+		printf("Result is NULL\n");
+	}
+
+	return 0;
+}
+*/
--- libft.h
+++ libft.h
@@ -40,4 +40,5 @@ void	*ft_calloc(size_t nelem, size_t elsize);
 char	*ft_strdup(char *src);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 #endif

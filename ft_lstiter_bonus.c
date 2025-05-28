1229180de937f41e6ff48d92c0ba61933c982376
Patrik <ptison@c2r7s1.42prague.com>
2025-05-28 19:24:30 +0200

    final touch

--- ft_lstiter_bonus.c
+++ ft_lstiter_bonus.c
@@ -31,3 +31,66 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 		lst = (*lst).next;
 	}
 }
+
+/*
+#include "libft.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void	to_uppercase(void *content)
+{
+	char	*str;
+
+	str = (char *)content;
+	while (*str)
+	{
+		*str = (char)toupper((unsigned char)*str);
+		str++;
+	}
+}
+
+void	print_list(t_list *lst)
+{
+	while (lst)
+	{
+		printf("Node: %s\n", (char *)lst->content);
+		lst = lst->next;
+	}
+}
+
+int	main(void)
+{
+	char	*s1;
+	char	*s2;
+	char	*s3;
+	t_list	*n1;
+	t_list	*n2;
+	t_list	*n3;
+	t_list	*head;
+
+	s1 = strdup("hello");
+	s2 = strdup("world");
+	s3 = strdup("list");
+	n1 = ft_lstnew(s1);
+	n2 = ft_lstnew(s2);
+	n3 = ft_lstnew(s3);
+	head = n1;
+	ft_lstadd_front(&head, n2);
+	ft_lstadd_front(&head, n3); // head → n3 → n2 → n1
+	printf("Before ft_lstiter:\n");
+	print_list(head);
+	ft_lstiter(head, to_uppercase);
+	printf("\nAfter ft_lstiter:\n");
+	print_list(head);
+	// Clean up
+	free(s1);
+	free(s2);
+	free(s3);
+	free(n1);
+	free(n2);
+	free(n3);
+	return (0);
+}
+*/
--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -53,3 +53,69 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	}
 	return (new_list);
 }
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "libft.h"
+
+void *to_uppercase_dup(void *content)
+{
+	char *str = (char *)content;
+	char *copy = strdup(str);
+	if (!copy)
+		return NULL;
+	for (int i = 0; copy[i]; i++)
+		copy[i] = (char)toupper((unsigned char)copy[i]);
+	return copy;
+}
+
+void del_content(void *content)
+{
+	free(content);
+}
+
+void print_list(t_list *lst)
+{
+	while (lst)
+	{
+		printf("Node: %s\n", (char *)lst->content);
+		lst = lst->next;
+	}
+}
+
+int	main(void)
+{
+	char *s1 = strdup("hello");
+	char *s2 = strdup("world");
+	char *s3 = strdup("list");
+
+	t_list *n1 = ft_lstnew(s1);
+	t_list *n2 = ft_lstnew(s2);
+	t_list *n3 = ft_lstnew(s3);
+
+	t_list *original = n1;
+	ft_lstadd_front(&original, n2);
+	ft_lstadd_front(&original, n3);
+
+	printf("Original list:\n");
+	print_list(original);
+
+	t_list *mapped = ft_lstmap(original, to_uppercase_dup, del_content);
+
+	printf("\nMapped list:\n");
+	print_list(mapped);
+
+	free(s1);
+	free(s2);
+	free(s3);
+	free(n1);
+	free(n2);
+	free(n3);
+	ft_lstclear(&mapped, del_content);
+
+	return 0;
+}
+*/
